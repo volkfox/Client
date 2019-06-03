ef9c01d6b5da2f609598f57ef2940c9221223e65
Daniel Kharitonov <volkfox@gmail.com>
2019-06-03 13:11:00 -0700

    Initial Commit

---
+++ Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Client
+//
+//  Created by Daniel Kharitonov on 6/3/19.
+//  Copyright © 2019 Daniel Kharitonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
